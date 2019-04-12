c6be276469d2aecbc2e534e9bfbb00cc604b610a
ehmatthes <eric@example.com>
2019-04-12 17:55:30 +0800

    Set DEBUG=False for Heroku.

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect, Http404
 from django.urls import reverse
 from .models import Topic, Entry
@@ -17,7 +17,7 @@ def topics(request):
 
 @login_required
 def topic(request,topic_id):
-    topic = Topic.objects.get(id=topic_id)
+    topic = get_object_or_404(Topic, id=topic_id)
     if topic.owner != request.user:
         raise Http404
     entries = topic.entry_set.order_by('-date_added')
